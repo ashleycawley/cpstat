a77d07b354f570469a13b3c984739bcb6543b145
Ashley Cawley <acawley@cloudabove.com>
2018-09-05 20:22:15 +0100

    Working example

--- cpstat.sh
+++ cpstat.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
 function NUMEMAIL {
-cat /home/$USER/.cpanel/email_accounts_count
+cp /home/$CPUSER/.cpanel/email_accounts_count /home/$CPUSER/.cpanel/email_accounts_count_readout
+echo -e $(cat /home/$CPUSER/.cpanel/email_accounts_count_readout)
+rm -f /home/$CPUSER/.cpanel/email_accounts_count_readout
 }
-
 
 # Script
 
-read -p "User: " USER
+echo && read -p "User: " CPUSER
 
+echo && echo "Scanning..." && echo
+
+echo -e "Email Accounts: \c"
 NUMEMAIL
+echo
 
 exit 0
