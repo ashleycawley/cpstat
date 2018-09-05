1e3af258080d6f25c450472edaf2d4976cbf3a5a
Ashley Cawley <acawley@cloudabove.com>
2018-09-05 20:35:32 +0100

    Added detection for number of databases and displaying of SSL Keys

--- cpstat.sh
+++ cpstat.sh
@@ -6,6 +6,14 @@ echo -e $(cat /home/$CPUSER/.cpanel/email_accounts_count_readout)
 rm -f /home/$CPUSER/.cpanel/email_accounts_count_readout
 }
 
+function SSLKEYS {
+echo -e $(cat /home/$CPUSER/.cpanel/nvdata/letsencrypt-cpanel)
+}
+
+function NUMDB {
+echo -e $(cat /home/$CPUSER/.cpanel/datastore/mysql-db-count)
+}
+
 # Script
 
 echo && read -p "User: " CPUSER
@@ -13,7 +21,13 @@ echo && read -p "User: " CPUSER
 echo && echo "Scanning..." && echo
 
 echo -e "Email Accounts: \c"
-NUMEMAIL
+NUMEMAIL && echo
+
+echo -e "MySQL Databases: \c"
+NUMDB && echo
+
+read -p "Press [Enter] to see the SSL Private Key, Certificate and more..."
 echo
+SSLKEYS
 
 exit 0
